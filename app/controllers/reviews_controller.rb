6aeeba36b3f02bd1cee96023082a3e990e2c88e3
BethThayne <beth@provenance.org>
2021-02-13 07:31:13 +0100

    added ambience and SEO

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -73,7 +73,7 @@ def update
 end
 
 def form_params
-    params.require(:review).permit(:title, :body, :score)
+    params.require(:review).permit(:title, :restaurant, :body, :score, :ambience)
 
 end
 
